c35641bef742dd21740deab3c58352353b478aca
Adrian Badarau <adi.badarau@yahoo.com>
2017-01-24 10:07:37 +0200

    added new controller

---
+++ src/AppBundle/Controller/GenusController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+
+class GenusController extends Controller
+{
+    /**
+     * @Route("/genus/{name}")
+     * @param string $name
+     * @return Response
+     */
+    public function showAction(string $name)
+    {
+        return new Response("ORKING " . $name);
+    }
+}
