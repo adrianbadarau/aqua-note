a554b521a6f4d0a29e1a53937a53101afd9c21f9
Adrian Badarau <adi.badarau@yahoo.com>
2017-01-24 16:09:03 +0200

    added doctrine entity class

---
+++ src/AppBundle/Entity/Genus.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Genus
+ *
+ * @ORM\Table(name="genus")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\GenusRepository")
+ */
+class Genus
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var $name string
+     * @ORM\Column(type="string",length=255)
+     */
+    private $name;
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     *
+     * @return Genus
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+}
