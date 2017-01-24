1ba0476d1dd30482f82328c92a1e103dd244f92e
Adrian Badarau <adi.badarau@yahoo.com>
2017-01-24 10:37:38 +0200

    added style

--- src/AppBundle/Controller/GenusController.php
+++ src/AppBundle/Controller/GenusController.php
@@ -15,6 +15,11 @@ class GenusController extends Controller
      */
     public function showAction(string $name)
     {
-        return new Response("ORKING " . $name);
+        $notes = [
+            'Lorem ipsum dolor sit amet, ne tantas dolorem quaestio quo, in vix choro principes torquatos. Eu qui iuvaret mentitum conclusionemque, quem laudem sententiae sea et. Eligendi ponderum dissentias ut cum, eu timeam audire scribentur ius! Eu eum veniam salutatus constituam. Omnesque petentium complectitur id sit. Vim reque saepe te, no nec natum persius.',
+            'Lorem ipsum dolor sit amet, ne tantas dolorem quaestio quo, in vix choro principes torquatos. Eu qui iuvaret mentitum conclusionemque, quem laudem sententiae sea et. Eligendi ponderum dissentias ut cum, eu timeam audire scribentur ius! Eu eum veniam salutatus constituam. Omnesque petentium complectitur id sit. Vim reque saepe te, no nec natum persius.',
+            'Lorem ipsum dolor sit amet, ne tantas dolorem quaestio quo, in vix choro principes torquatos. Eu qui iuvaret mentitum conclusionemque, quem laudem sententiae sea et. Eligendi ponderum dissentias ut cum, eu timeam audire scribentur ius! Eu eum veniam salutatus constituam. Omnesque petentium complectitur id sit. Vim reque saepe te, no nec natum persius.'
+        ];
+        return $this->render("@App/genus/show.html.twig",compact('name', 'notes'));
     }
 }
