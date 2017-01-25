9a4867e4b2c97a250b4e22eceb2303e79f4a17da
Adrian Badarau <adi.badarau@yahoo.com>
2017-01-25 23:10:48 +0200

    refactored forms to use partials

--- src/AppBundle/Controller/Admin/GenusAdminController.php
+++ src/AppBundle/Controller/Admin/GenusAdminController.php
@@ -2,6 +2,7 @@
 
 namespace AppBundle\Controller\Admin;
 
+use AppBundle\Entity\Genus;
 use AppBundle\Form\GenusFormType;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
@@ -45,7 +46,32 @@ class GenusAdminController extends Controller
             return $this->redirectToRoute('admin_genus_list');
         }
         return $this->render('AppBundle:admin/genus:new.html.twig', [
-            'genusForm' => $form->createView()
+            'genusForm' => $form->createView(),
+            'type' => 'New'
+        ]);
+    }
+
+    /**
+     * @Route("/genus/{id}/edit", name="admin_genus_edit")
+     * @param Genus $genus
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function editAction(Request $request,Genus $genus)
+    {
+        $form = $this->createForm(GenusFormType::class, $genus);
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()){
+            $genus = $form->getData();
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($genus);
+            $em->flush();
+            $this->addFlash('success', 'Genus successfully edited');
+            return $this->redirectToRoute('admin_genus_list');
+        }
+        return $this->render('AppBundle:admin/genus:new.html.twig',[
+            'genus' => $genus,
+            'genusForm' => $form->createView(),
+            'type' => 'Edit'
         ]);
     }
 }
